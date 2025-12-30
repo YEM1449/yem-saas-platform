c1f10836cafdc339e7a0f2b5475c91e700df2a0d
YEM <youssouf.mehdi@gmail.Com>
2025-12-30 02:12:05 +0100

    task-archi-01

---
+++ hlm-backend/src/main/java/com/yem/hlm/backend/HlmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yem.hlm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlmBackendApplication.class, args);
+	}
+
+}
---
+++ hlm-backend/src/test/java/com/yem/hlm/backend/HlmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yem.hlm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
