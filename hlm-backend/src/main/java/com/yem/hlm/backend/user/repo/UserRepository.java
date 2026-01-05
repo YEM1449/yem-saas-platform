cf5ba6e6d5c826ff540f25e572923500ff2dfaa4
YEM <youssouf.mehdi@gmail.Com>
2026-01-05 23:55:02 +0100

    Parametrage DB connexion + liquibase

--- hlm-backend/src/main/java/com/yem/hlm/backend/user/repo/UserRepository.java
+++ hlm-backend/src/main/java/com/yem/hlm/backend/user/repo/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByTenantAndEmail(UUID tenantId, String email);
+    Optional<User> findByTenant_IdAndEmail(UUID tenantId, String email);
 }
